194290c3dee26ae0b1184fa9ce6581cb7d7f13a7
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-08-02 11:25:13 +0500

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FaGear, FaMicrophone, FaAngleLeft } from 'react-icons/fa6';
+import { Dropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div className="container">
+      <FaAngleLeft className="mr-auto" />
+      <div className="form-group mr-auto">
+        <Dropdown>
+          <Dropdown.Toggle variant="primary" id="dropdown-primary">
+            2023
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item>2023</Dropdown.Item>
+            <Dropdown.Item>2022</Dropdown.Item>
+            <Dropdown.Item>2021</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+
+      <Link className="navbar-brand mx-auto" to="/">Title</Link>
+
+      <ul className="navbar-nav ml-auto" style={{ display: 'contents' }}>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            <FaMicrophone />
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            <FaGear />
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default Navbar;
